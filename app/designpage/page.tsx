ba00f147937ed3d86bdb605c9489cc1a182f490a
touxy <touxhk@gmail.com>
2026-01-13 13:34:38 +0700

    update 13.1.2025

--- app/designpage/page.tsx
+++ app/designpage/page.tsx
@@ -1,10 +1,19 @@
+"use client"
 import DesignImg from "@/component/designimg"
+import { useState } from "react";
+import { MdOutlineDarkMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
+
 
 
 export default function DesignPage(){
+    const [mode2, setMode2] = useState(true);
     return(
-        <div>
-            <h1 className="text-center text-4xl font-bold">Designing Performance</h1>
+        <div style={mode2 === true ? {backgroundColor:"white", color:"black"} : {backgroundColor:"#141414", color:"white"}}>
+            <button  
+            
+             onClick={() =>setMode2(!mode2)}>{mode2 === true ? <MdDarkMode size={30} /> : <MdOutlineDarkMode size={30} />}</button>
+            <h1 className="text-center text-4xl font-bold ">Designing Performance</h1>
             <DesignImg />
         </div>
     )
--- component/designimg.tsx
+++ component/designimg.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react"
 
 
 
 
 export default function DesignImg() {
+    const [imgclickzoom, setclickzoom] = useState(false);
 
     const dsimg = [
         "/dsimg1.png",
@@ -24,7 +26,12 @@ export default function DesignImg() {
         <div>
             <div className="flex flex-wrap gap-10 justify-center p-8">
                 {dsimg.map((src, index) => (
-                    <img key={index} src={src} alt="Design Image" className="w-[400px]" />
+                    <button onClick={() => setclickzoom(!imgclickzoom) } key={index} className="border-2 border-blue-400 cursor-pointer ">
+                        <img
+                        style={imgclickzoom === true ? {width: "800px"} : {width: "400px"}}
+                            key={index}
+                            src={src} alt="Design Image"
+                            className="w-[400px] hover:shadow-2xl shadow-gray-600 hover:scale-105" /></button>
                 ))}
             </div>
         </div>
